1c32a4228deb8b2e714095f0f887ac181c9c1036
shravan-saini <shravansaini94@gmail.com>
2017-08-20 10:48:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articleName',function(req,res)    {
-    res.send(createTemplate(articles[articleName]));
+    res.send(createTemplate(articles[articleone]));
 });
 
 
