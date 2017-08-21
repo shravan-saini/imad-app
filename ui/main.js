377ce088444ab940d966b8d52b96bf5a08cf630f
shravan-saini <shravansaini94@gmail.com>
2017-08-21 10:52:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,29 @@
 
-
+// code for counter endpoint
 console.log('Loaded!');
+var button = document.getElementById('counter');
+console.log('hi');
+button.onclick = function() {
+    // Create an request object
+    var request = new XMLHttpRequest();
+    
+    //Capture the response and store it in a variable
+    
+    request.onreadystatechange = function() {   
+        if(request.readyState === XMLHttpRequest.DONE)
+        {
+            if(request.status===200)
+            {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+    
+    //Make an request
+    request.open('GET','http://shravansaini94.imad.hasura-app.io/counter',true);
+    request.send(null);
+    
+};
 
