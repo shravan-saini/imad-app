85513c8dd0709218f18cdfa52c1b8841e831ea29
shravan-saini <shravansaini94@gmail.com>
2017-08-21 08:51:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,35 @@
+var button = getElementById('counter');
+
+button.onclick = function() {
+    // Make an request object
+    var request = new XMLHttpRequest();
+    
+    //Capture the response and store it in a variable
+    if(request.readyState === XMLHttpRequest.DONE)
+    {
+        if(request.status===200)
+        {
+            var counter = request.responseText;
+            var span = getElementById('count');
+            span.innerHTML = counter.toString();
+        }
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 console.log('Loaded!');
+
