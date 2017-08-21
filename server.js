f2f0e1208fa889fe551306ac0acd0c70062ce412
shravan-saini <shravansaini94@gmail.com>
2017-08-21 10:52:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.get('/counter', function(req,res)   {
 
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/:articleNames',function(req,res)    {
