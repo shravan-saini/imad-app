dd9112e02f46f89d162b061eca14f233be542fd6
shravan-saini <shravansaini94@gmail.com>
2017-08-30 12:41:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var config = {
     database:'shravansaini94',
     host :'http://db.imad.hasura-app.io',
     port  :'5432',
-    password:process.env.DB_PASSWORD
+    password:'db-shravansaini94-99413'
     
 };
 
