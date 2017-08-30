a4eeabc116b765ea388ed1d56d4cc1fda859d18a
shravan-saini <shravansaini94@gmail.com>
2017-08-30 13:54:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/testdb',function(req,res){
    pool.query('SELECT * FROM test',function(err,result){
        if(err)
        {
-           res.status(500).send(err.toString());
+           res.status(502).send(err.toString());
        }
        else
         {
