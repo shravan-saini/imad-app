a7c6a903c85f23cf3e92f997c7876263d8704dc4
shravan-saini <shravansaini94@gmail.com>
2017-08-30 13:51:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,32 +15,16 @@ var config = {
 
 //creating a pool connection
 var pool = new Pool(config);
-app.get('/test-db',function(req,res){
+app.get('/testdb',function(req,res){
    
    pool.query('SELECT *FROM test',function(err,result){
-       if(err)
-       {
-           res.status(509).send(err.toString());
-       }
-       else
+       
         {
             res.send(JSON.stringify(result));
         }
    });
   
 });
-
-
-
-
-
-
-
-
-
-
-
-
 
 var counter=0;
 app.get('/counter', function(req,res)   {
