3e2a0d7938e3dd8c1539140f924b2529fff84710
shravan-saini <shravansaini94@gmail.com>
2017-08-30 17:08:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ var pool = new Pool(config);
 
 app.get('/articles/:articleName',function(req,res){
    
-   pool.query("SELECT * FROM articles WHERE title='"+req.params.articleName+"'",function(err,result){
+   pool.query("SELECT * FROM articles WHERE title=$1",[req.params.articleName],function(err,result){
        if(err)
        {
            res.status(509).send(err.toString());
