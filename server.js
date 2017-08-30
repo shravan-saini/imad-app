9dd16281102ecff1151db1c41b9fd8b7cc405eb0
shravan-saini <shravansaini94@gmail.com>
2017-08-30 17:03:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.get('/articles/:articleName',function(req,res){
        }
        else
         {
-            if(result.rwos.length===0)
+            if(result.rows.length===0)
             {
                 res.send(404).send("Article Not Found");
             }
