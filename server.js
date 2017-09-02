9a297e9892811891a1fec5e80789dc8538106b4e
shravan-saini <shravansaini94@gmail.com>
2017-09-02 18:57:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -107,6 +107,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/articles.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'articles.css'));
+});
+
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
