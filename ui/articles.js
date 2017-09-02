3ac1fcb78b61f21757a2cf2f347180f0857f1878
shravan-saini <shravansaini94@gmail.com>
2017-09-02 20:33:58 +0530

    [imad-console] Updates ui/articles.js

--- ui/articles.js
+++ ui/articles.js
@@ -1,2 +1,32 @@
+// crate an request to server
+var request = new XMLHttpRequest();
+
+// capture the response in a variable
+request.onreadystatechange = function()   {
+  if(request.readyState === XMLHttpRequest.DONE)
+  {
+      if(request.status===200)
+      {
+          var articleList = request.responseText;
+          articleLists = JSON.parse(articleList);
+          var list='';
+          if(articleList.length<5)
+              for(var i=0;i<articleList.length;i++)
+              {
+                  list+= '<li>'+names[i]+'</li>';
+              }
+            else
+              for(i=0;i<5;i++)
+              {
+                  list+= '<li>'+names[i]+'</li>';
+              }
+          var ul = document.getElementById('newArticleList');
+          ul.innerHTML = list;
+          
+      }
+  }
+};
+request.open('GET','http://shravansaini94.imad.hasura-app.io/GetNewArticles',true);
+request.send(null);
 
 console.log('hello');
